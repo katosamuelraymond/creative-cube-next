79e26099fe0fb26013c94bf44fefa46708ee2d3d
katosamuelraymond <katosamuelraymondmarvinhosborn@gmail.com>
2026-05-24 22:17:38 +0300

    use local prisma binary

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-npx prisma migrate deploy --schema=prisma/schema.prisma
+node_modules/.bin/prisma migrate deploy --schema=prisma/schema.prisma
 
 echo "Starting Next.js..."
 exec node server.js
